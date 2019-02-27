f8e13c7de2ee1bf9f2125941748e12e10bb63892
bruno.cruz <brunovieiracrz@gmail.com>
2019-02-27 14:17:50 -0300

    add logic to activate application on sdk

--- app/src/main/java/com/example/paymentstone/ui/activities/HomeActivity.kt
+++ app/src/main/java/com/example/paymentstone/ui/activities/HomeActivity.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.paymentstone.R
 import com.example.paymentstone.commons.bindView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import stone.application.interfaces.StoneCallbackInterface
 import stone.environment.Environment
+import stone.providers.ActiveApplicationProvider
 
 class HomeActivity : AppCompatActivity() {
 
@@ -22,16 +25,27 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         spinner.adapter = recoverSpinnerAdapter()
-        fabDone.setOnClickListener {
-            startActivity(Intent(this, DashboardActivity::class.java))
-        }
+        fabDone.setOnClickListener { activateApplication() }
     }
 
     private fun recoverSpinnerAdapter(): ArrayAdapter<String> {
-        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item)
-        for (environment: Environment in Environment.values()) {
-            arrayAdapter.add(environment.name)
-        }
-        return arrayAdapter
+        val environmentNameList = Environment.values().map { it.name }
+        return ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, environmentNameList)
+    }
+
+    private fun activateApplication() {
+        ActiveApplicationProvider(this).apply {
+            useDefaultUI(true)
+            dialogTitle = context.getString(R.string.loading)
+            connectionCallback = object : StoneCallbackInterface {
+                override fun onSuccess() {
+                    startActivity(Intent(this@HomeActivity, DashboardActivity::class.java))
+                }
+
+                override fun onError() {
+                    Toast.makeText(context, R.string.home_activation_error, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }.activate(editText.text.toString())
     }
 }
