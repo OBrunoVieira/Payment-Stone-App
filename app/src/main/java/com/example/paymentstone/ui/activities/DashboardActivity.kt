adc409d91331b49c062dfeab08236ccfe4e1382e
bruno.cruz <brunovieiracrz@gmail.com>
2019-03-01 17:35:37 -0300

    create presentation logic designed to indicate pinpad status

--- app/src/main/java/com/example/paymentstone/ui/activities/DashboardActivity.kt
+++ app/src/main/java/com/example/paymentstone/ui/activities/DashboardActivity.kt
@@ -4,14 +4,16 @@ import android.bluetooth.BluetoothAdapter
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import com.example.paymentstone.R
 import com.example.paymentstone.commons.bindView
-import com.example.paymentstone.ui.views.BluetoothDialog
+import com.example.paymentstone.ui.views.DevicesDialog
 import com.example.paymentstone.ui.views.MessageDialog
 import stone.application.interfaces.StoneCallbackInterface
 import stone.providers.ActiveApplicationProvider
@@ -22,11 +24,14 @@ class DashboardActivity : AppCompatActivity() {
     private val cardViewTransaction by bindView<CardView>(R.id.dashboard_card_view_transaction)
     private val cardViewMessage by bindView<CardView>(R.id.dashboard_card_view_message)
     private val cardViewExit by bindView<CardView>(R.id.dashboard_card_view_logout)
+    private val textViewPinPadStatus by bindView<TextView>(R.id.dashboard_text_view_pin_pad_status)
     private val buttonConnect by bindView<Button>(R.id.dashboard_button_pin_pad_action)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        checkPinPadStatus()
 
         cardViewTransaction.setOnClickListener {
             executeAfterPinPadConnection(fun() {
@@ -67,8 +72,27 @@ class DashboardActivity : AppCompatActivity() {
                     startActivity(Intent().setAction(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS))
                 })
             } else {
-                BluetoothDialog(this).show()
+                val bluetoothDialog = DevicesDialog(this)
+                bluetoothDialog.setOnDismissListener { checkPinPadStatus() }
+                bluetoothDialog.show()
             }
+        }
+    }
+
+    private fun checkPinPadStatus() {
+        val isPinPadConnected = isPinPadConnected()
+        textViewPinPadStatus.apply {
+            setText(if (isPinPadConnected) R.string.dashboard_pin_pad_connected else R.string.dashboard_pin_pad_not_connected)
+            setCompoundDrawablesWithIntrinsicBounds(
+                    if (isPinPadConnected) R.drawable.shape_circle_green else R.drawable.shape_circle_gray,
+                    0, 0, 0
+            )
+        }
+
+        buttonConnect.apply {
+            setText(if (isPinPadConnected) R.string.dashboard_disconnect else R.string.dashboard_connect )
+            setTextColor(ContextCompat.getColor(context, if (isPinPadConnected) R.color.pomegranate else R.color.atlantis ))
+            setBackgroundResource(if (isPinPadConnected)  R.drawable.shape_outline_red else R.drawable.shape_outline_green)
         }
     }
 
