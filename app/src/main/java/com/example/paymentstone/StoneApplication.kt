bfa6249761d65cff16ab812dff6d3135cc964e75
bruno.cruz <brunovieiracrz@gmail.com>
2019-03-11 19:09:01 -0300

    create single reference of application

--- app/src/main/java/com/example/paymentstone/StoneApplication.kt
+++ app/src/main/java/com/example/paymentstone/StoneApplication.kt
@@ -1,12 +1,18 @@
 package com.example.paymentstone
 
 import android.app.Application
-import stone.application.StoneStart
 import stone.utils.Stone
 
 class StoneApplication : Application() {
+
+    companion object {
+        lateinit var instance: StoneApplication
+            private set
+    }
+
     override fun onCreate() {
         super.onCreate()
+        instance = this
         Stone.setAppName(getString(R.string.app_name))
     }
 }
