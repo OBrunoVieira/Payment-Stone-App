d3a0f8150e02be36a42cc57a5fbb817f3624120e
bruno.cruz <brunovieiracrz@gmail.com>
2019-03-12 15:29:38 -0300

    move behaviour to specific methods

--- app/src/main/java/com/example/paymentstone/ui/activities/DashboardActivity.kt
+++ app/src/main/java/com/example/paymentstone/ui/activities/DashboardActivity.kt
@@ -3,9 +3,11 @@ package com.example.paymentstone.ui.activities
 import android.bluetooth.BluetoothAdapter
 import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +19,7 @@ import com.example.paymentstone.ui.views.DevicesDialog
 import com.example.paymentstone.ui.views.MessageDialog
 import stone.application.interfaces.StoneCallbackInterface
 import stone.providers.ActiveApplicationProvider
+import stone.providers.ReversalProvider
 import stone.utils.Stone
 
 class DashboardActivity : AppCompatActivity() {
@@ -24,6 +27,7 @@ class DashboardActivity : AppCompatActivity() {
     private val cardViewTransaction by bindView<CardView>(R.id.dashboard_card_view_transaction)
     private val cardViewMessage by bindView<CardView>(R.id.dashboard_card_view_message)
     private val cardViewExit by bindView<CardView>(R.id.dashboard_card_view_logout)
+    private val cardViewErrorTransaction by bindView<CardView>(R.id.dashboard_card_view_error_transaction)
     private val cardViewTransactionList by bindView<CardView>(R.id.dashboard_card_view_transaction_list)
     private val textViewPinPadStatus by bindView<TextView>(R.id.dashboard_text_view_pin_pad_status)
     private val buttonConnect by bindView<Button>(R.id.dashboard_button_pin_pad_action)
@@ -31,8 +35,6 @@ class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
-
-        checkPinPadStatus()
 
         cardViewTransaction.setOnClickListener {
             executeAfterPinPadConnection(fun() {
@@ -46,42 +48,18 @@ class DashboardActivity : AppCompatActivity() {
             })
         }
 
-        cardViewTransactionList.setOnClickListener {
-            startActivity(Intent(this, TransactionListActivity::class.java))
-        }
+        cardViewTransactionList.setOnClickListener { startActivity(Intent(this, TransactionListActivity::class.java)) }
 
-        cardViewExit.setOnClickListener {
-            showActionDialog(R.string.logout_confirmation, action = fun() {
-                ActiveApplicationProvider(this).apply {
-                    useDefaultUI(true)
-                    dialogTitle = getString(R.string.loading)
-                    dialogMessage = getString(R.string.logout_feedback)
-                    connectionCallback = object : StoneCallbackInterface {
-                        override fun onSuccess() {
-                            startActivity(Intent(context, HomeActivity::class.java))
-                            finish()
-                        }
+        cardViewErrorTransaction.setOnClickListener { setupErrorTransaction() }
 
-                        override fun onError() {
-                            Toast.makeText(context, R.string.logout_error, Toast.LENGTH_SHORT).show()
-                        }
+        cardViewExit.setOnClickListener { setupExit() }
 
-                    }
-                }.deactivate()
-            })
-        }
+        buttonConnect.setOnClickListener { setupConnectButton() }
+    }
 
-        buttonConnect.setOnClickListener {
-            if (!BluetoothAdapter.getDefaultAdapter().isEnabled) {
-                showActionDialog(R.string.bluetooth_disabled, R.string.settings, fun() {
-                    startActivity(Intent().setAction(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS))
-                })
-            } else {
-                val bluetoothDialog = DevicesDialog(this)
-                bluetoothDialog.setOnDismissListener { checkPinPadStatus() }
-                bluetoothDialog.show()
-            }
-        }
+    override fun onResume() {
+        super.onResume()
+        checkPinPadStatus()
     }
 
     private fun checkPinPadStatus() {
@@ -113,8 +91,59 @@ class DashboardActivity : AppCompatActivity() {
         }
     }
 
+    private fun setupConnectButton() {
+        if (!BluetoothAdapter.getDefaultAdapter().isEnabled) {
+            showActionDialog(R.string.bluetooth_disabled, R.string.settings, fun() {
+                startActivity(Intent().setAction(Settings.ACTION_BLUETOOTH_SETTINGS))
+            })
+        } else {
+            val bluetoothDialog = DevicesDialog(this)
+            bluetoothDialog.setOnDismissListener { checkPinPadStatus() }
+            bluetoothDialog.show()
+        }
+    }
+
+    private fun setupExit() {
+        showActionDialog(R.string.logout_confirmation, action = fun() {
+            ActiveApplicationProvider(this).apply {
+                useDefaultUI(true)
+                dialogTitle = getString(R.string.loading)
+                dialogMessage = getString(R.string.logout_feedback)
+                connectionCallback = object : StoneCallbackInterface {
+                    override fun onSuccess() {
+                        startActivity(Intent(context, HomeActivity::class.java))
+                        finish()
+                    }
+
+                    override fun onError() {
+                        Toast.makeText(context, R.string.logout_error, LENGTH_SHORT).show()
+                    }
+
+                }
+            }.deactivate()
+        })
+    }
+
+    private fun setupErrorTransaction() {
+        showActionDialog(R.string.transaction_error_confirmation, action = fun() {
+            ReversalProvider(this).apply {
+                dialogMessage = getString(R.string.transaction_error_loading)
+                connectionCallback = object : StoneCallbackInterface {
+                    override fun onSuccess() {
+                        Toast.makeText(this@DashboardActivity, R.string.transaction_error_got_success, LENGTH_SHORT).show()
+                    }
+
+                    override fun onError() {
+                        Toast.makeText(this@DashboardActivity, R.string.transaction_error_got_fail, LENGTH_SHORT).show()
+                    }
+
+                }
+            }.execute()
+        })
+    }
+
     private fun showActionDialog(@StringRes message: Int,
-                                 @StringRes positiveText: Int = R.string.ok,
+                                 @StringRes positiveText: Int = R.string.yes,
                                  action: () -> Unit) {
         AlertDialog.Builder(this)
                 .setMessage(message)
